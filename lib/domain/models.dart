8d9a3feeaf224d6e99392e75601ab3b602a466c6
shehab nabil <shehabnabil790@gmail.com>
2023-12-13 00:08:26 +0200

    creating models for login

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -20,3 +20,35 @@ class SliderViewObjecModel {
     this.currentIndex,
   );
 }
+// login model
+
+class CustomerModel {
+  String id;
+  String name;
+  int numOfNotifications;
+  CustomerModel(
+    this.id,
+    this.name,
+    this.numOfNotifications,
+  );
+}
+
+class ContactsModel {
+  String phone;
+  String email;
+  String link;
+  ContactsModel(
+    this.phone,
+    this.email,
+    this.link,
+  );
+}
+
+class AuthenticationModel {
+  CustomerModel customer;
+  ContactsModel contacts;
+  AuthenticationModel(
+    this.customer,
+    this.contacts,
+  );
+}
