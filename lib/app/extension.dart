dd7e6cc0b88c02f5db3dbf77cdb03920d2794553
shehab nabil <shehabnabil790@gmail.com>
2023-12-13 23:47:05 +0200

    add extensions

---
+++ lib/app/extension.dart
@@ -0,0 +1,19 @@
+extension NonNullStrings on String? {
+  String orEmpty() {
+    if (this == null) {
+      return '';
+    } else {
+      return this!;
+    }
+  }
+}
+
+extension NonNullInt on int? {
+  int orZero() {
+    if (this == null) {
+      return 0;
+    } else {
+      return this!;
+    }
+  }
+}
